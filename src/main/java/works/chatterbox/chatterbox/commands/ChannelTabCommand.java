e84c6d94bc769a05fd9073b7c61723cb6303b749
Kyle Clemens <jkc.clemens@gmail.com>
2015-05-19 16:13:14 -0400

    Added: JoinCommand and LeaveCommand implementations

---
+++ src/main/java/works/chatterbox/chatterbox/commands/ChannelTabCommand.java
@@ -0,0 +1,27 @@
+package works.chatterbox.chatterbox.commands;
+
+import com.google.common.collect.Lists;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import works.chatterbox.chatterbox.Chatterbox;
+import works.chatterbox.chatterbox.commands.TabCommand;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+public abstract class ChannelTabCommand extends TabCommand<Chatterbox> {
+
+    protected ChannelTabCommand(final Chatterbox instance, final String name, final boolean checkPermissions, final Short[] cts) {
+        super(instance, name, checkPermissions, Stream.concat(Arrays.stream(cts), Arrays.stream(new Short[]{CompletionType.LIST.getShort()})).toArray(Short[]::new));
+    }
+
+    @Override
+    protected List<String> customList(final CommandSender cs, final Command cmd, final String label, final String[] args, final String arg) {
+        final Collection<String> namesAndTags = this.plugin.getAPI().getChannelAPI().getAllChannelNames();
+        namesAndTags.addAll(this.plugin.getAPI().getChannelAPI().getAllChannelTags());
+        return Lists.newArrayList(namesAndTags);
+    }
+
+}
--- src/main/java/works/chatterbox/chatterbox/commands/impl/JoinCommand.java
+++ src/main/java/works/chatterbox/chatterbox/commands/impl/JoinCommand.java
@@ -1,4 +1,40 @@
 package works.chatterbox.chatterbox.commands.impl;
 
-public class JoinCommand {
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import works.chatterbox.chatterbox.Chatterbox;
+import works.chatterbox.chatterbox.channels.Channel;
+import works.chatterbox.chatterbox.commands.ChannelTabCommand;
+import works.chatterbox.chatterbox.wrappers.CPlayer;
+
+public class JoinCommand extends ChannelTabCommand {
+
+    protected JoinCommand(final Chatterbox instance, final String name) {
+        super(instance, name, true, new Short[0]);
+    }
+
+    @Override
+    public boolean runCommand(final CommandSender cs, final Command cmd, final String label, final String[] eargs, final CommandArguments ca) {
+        if (eargs.length < 1 || !(cs instanceof Player)) {
+            cs.sendMessage(cmd.getDescription());
+            return false;
+        }
+        final Player player = (Player) cs;
+        final String channelName = eargs[0];
+        final Channel channel = this.plugin.getAPI().getChannelAPI().getChannel(channelName);
+        if (channel == null) {
+            cs.sendMessage(ChatColor.RED + this.plugin.getLanguage().getString("NO_SUCH_CHANNEL"));
+            return true;
+        }
+        final CPlayer cp = this.plugin.getAPI().getPlayerAPI().getCPlayer(player);
+        if (cp.getChannels().contains(channel)) {
+            cs.sendMessage(ChatColor.RED + this.plugin.getLanguage().getString("ALREADY_IN_CHANNEL"));
+            return true;
+        }
+        cp.joinChannel(channel);
+        cs.sendMessage(ChatColor.BLUE + this.plugin.getLanguage().getFormattedString("JOINED_CHANNEL", ChatColor.GRAY + channel.getName() + ChatColor.BLUE));
+        return true;
+    }
 }
