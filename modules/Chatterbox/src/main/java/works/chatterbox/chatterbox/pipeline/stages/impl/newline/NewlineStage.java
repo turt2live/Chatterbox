830ef40a17b7a8c48cbc12d6b6e5d631eba0b868
Kyle Clemens <jkc.clemens@gmail.com>
2015-05-26 00:05:04 -0400

    Fixed: Workaround for #5
    This trims every line, then replaces %w with one space (not %s, for fear of messing up String.format). May want to move away from % in general because of this.

--- modules/Chatterbox/src/main/java/works/chatterbox/chatterbox/pipeline/stages/impl/newline/NewlineStage.java
+++ modules/Chatterbox/src/main/java/works/chatterbox/chatterbox/pipeline/stages/impl/newline/NewlineStage.java
@@ -10,11 +10,19 @@ import works.chatterbox.chatterbox.messages.Message;
 import works.chatterbox.chatterbox.pipeline.PipelineContext;
 import works.chatterbox.chatterbox.pipeline.stages.Stage;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class NewlineStage implements Stage {
 
     @Override
     public void process(@NotNull final Message message, @NotNull final PipelineContext context) {
         if (message.isCancelled()) return;
-        message.setFormat(message.getFormat().replace("\n", "").replace("%n", "\n"));
+        final String newlined = message.getFormat().replace("\n", "").replace("%n", "\n");
+        message.setFormat(
+            Arrays.stream(newlined.split("\\n"))
+                .map(part -> part.trim().replace("%w", " "))
+                .collect(Collectors.joining("\n"))
+        );
     }
 }
