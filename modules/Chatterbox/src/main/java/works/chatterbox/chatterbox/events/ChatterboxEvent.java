04d3177c911a789877a56eb1277eeeb8611cd285
Kyle Clemens <jkc.clemens@gmail.com>
2015-05-29 17:05:51 -0400

    Fixed: Haters gonna hate

--- modules/Chatterbox/src/main/java/works/chatterbox/chatterbox/events/ChatterboxEvent.java
+++ modules/Chatterbox/src/main/java/works/chatterbox/chatterbox/events/ChatterboxEvent.java
@@ -5,15 +5,25 @@
  */
 package works.chatterbox.chatterbox.events;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
 
 public abstract class ChatterboxEvent extends Event {
 
-    private final static HandlerList handlerList = new HandlerList();
+    private final static LoadingCache<String, HandlerList> handlerLists = CacheBuilder.newBuilder()
+        .build(new CacheLoader<String, HandlerList>() {
+            @Override
+            public HandlerList load(@NotNull final String key) throws Exception {
+                return new HandlerList();
+            }
+        });
 
     @Override
     public HandlerList getHandlers() {
-        return ChatterboxEvent.handlerList;
+        return ChatterboxEvent.handlerLists.getUnchecked(this.getClass().getName());
     }
 }
