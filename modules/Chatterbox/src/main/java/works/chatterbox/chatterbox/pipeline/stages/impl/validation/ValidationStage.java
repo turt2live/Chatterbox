f3948e8a243e7cdc997c8581179c436898cd80ef
Kyle Clemens <jkc.clemens@gmail.com>
2015-05-31 12:17:03 -0400

    Fixed: Actually add the stage, idiot

---
+++ modules/Chatterbox/src/main/java/works/chatterbox/chatterbox/pipeline/stages/impl/validation/ValidationStage.java
@@ -0,0 +1,22 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package works.chatterbox.chatterbox.pipeline.stages.impl.validation;
+
+import org.jetbrains.annotations.NotNull;
+import works.chatterbox.chatterbox.messages.Message;
+import works.chatterbox.chatterbox.pipeline.PipelineContext;
+import works.chatterbox.chatterbox.pipeline.stages.Stage;
+
+public class ValidationStage implements Stage {
+
+    @Override
+    public void process(@NotNull final Message message, @NotNull final PipelineContext context) {
+        // No empty messages!
+        if (message.getMessage().isEmpty()) {
+            message.setCancelled(true);
+        }
+    }
+}
