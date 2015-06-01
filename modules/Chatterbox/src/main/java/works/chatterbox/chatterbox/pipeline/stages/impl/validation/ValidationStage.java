8f2fd6785564984c63ef99576ab0e4d36f481f89
Kyle Clemens <jkc.clemens@gmail.com>
2015-05-31 22:14:11 -0400

    Fixed: Don't block JSON

--- modules/Chatterbox/src/main/java/works/chatterbox/chatterbox/pipeline/stages/impl/validation/ValidationStage.java
+++ modules/Chatterbox/src/main/java/works/chatterbox/chatterbox/pipeline/stages/impl/validation/ValidationStage.java
@@ -7,6 +7,7 @@ package works.chatterbox.chatterbox.pipeline.stages.impl.validation;
 
 import org.jetbrains.annotations.NotNull;
 import works.chatterbox.chatterbox.messages.Message;
+import works.chatterbox.chatterbox.messages.PlayerMessage;
 import works.chatterbox.chatterbox.pipeline.PipelineContext;
 import works.chatterbox.chatterbox.pipeline.stages.Stage;
 
@@ -14,8 +15,8 @@ public class ValidationStage implements Stage {
 
     @Override
     public void process(@NotNull final Message message, @NotNull final PipelineContext context) {
-        // No empty messages!
-        if (message.getMessage().isEmpty()) {
+        // No empty messages from players!
+        if (message instanceof PlayerMessage && message.getMessage().isEmpty()) {
             message.setCancelled(true);
         }
     }
